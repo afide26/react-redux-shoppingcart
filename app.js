92b0106bca07d48ddfa89eb5a29781c14d4926fe
Alan Fidelino <afidedev26@gmail.com>
2017-07-30 20:59:10 +1000

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // APIs server code
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/bookshop');
+mongoose.connect('mongodb://localhost:27017/bookshop', {useMongoClient:true});
 
 
 var Books = require('./models/books.js');
